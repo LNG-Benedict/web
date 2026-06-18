768c1ac52ea4b6a41dd4a26fd1e959946b17c0e4
LNG-Benedict <darrelllingga@gmail.com>
2026-06-18 22:35:49 +0700

    Add files via upload

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ const mulaiBtn = document.getElementById("mulai");
 const halamanAwal = document.getElementById("halaman-awal");
 const halamanUtama = document.getElementById("halaman-utama");
 const musik = document.getElementById("musik");
+const card = document.querySelector(".card");
+const selesai = document.getElementById("selesai");
 
 mulaiBtn.addEventListener("click", function(){
   halamanAwal.style.opacity = "0";
@@ -12,7 +14,19 @@ mulaiBtn.addEventListener("click", function(){
       halamanUtama.style.visibility = "visible";
       halamanUtama.style.opacity = "1";
       
+
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
     }, 500);
     musik.play()
   }, 500);
+});
+
+selesai.addEventListener("click", function(){
+  const nomor = "6283838047138";
+  const pesan = "Sudah dibaca yaa....";
+
+  const url = `https://wa.me/${nomor}?text=${encodeURIComponent(pesan)}`;
+
+  window.open(url, "_blank");
 });
